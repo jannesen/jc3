8fa2f14ffb13fb31c264b17290fdf9cb192c8070
Peter Jannesen <peter@jannesen.com>
2019-07-15 19:39:49 +0200

    jannesen.async: Add extra type to Task.all

--- src/jannesen.async.ts
+++ src/jannesen.async.ts
@@ -518,6 +518,7 @@ export class Task<T> implements Promise<T>,PromiseLike<T>,TaskInspection<T>
     /**
      * !!DOC
      */
+    public static       all(values:(PromiseLike<void>|undefined)[]): Task<void>;
     public static       all<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(values: [T1 | PromiseLike<T1>,T2 | PromiseLike<T2>,T3 | PromiseLike<T3>,T4 | PromiseLike<T4>,T5 | PromiseLike<T5>,T6 | PromiseLike<T6>,T7 | PromiseLike<T7>,T8 | PromiseLike<T8>,T9 | PromiseLike<T9>,T10 | PromiseLike<T10>]): Task<[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10]>;
     public static       all<T1, T2, T3, T4, T5, T6, T7, T8, T9>(values: [T1 | PromiseLike<T1>, T2 | PromiseLike<T2>, T3 | PromiseLike<T3>, T4 | PromiseLike<T4>, T5 | PromiseLike<T5>, T6 | PromiseLike<T6>, T7 | PromiseLike<T7>, T8 | PromiseLike<T8>, T9 | PromiseLike<T9>]): Task<[T1, T2, T3, T4, T5, T6, T7, T8, T9]>;
     public static       all<T1, T2, T3, T4, T5, T6, T7, T8>(values: [T1 | PromiseLike<T1>, T2 | PromiseLike<T2>, T3 | PromiseLike<T3>, T4 | PromiseLike<T4>, T5 | PromiseLike<T5>, T6 | PromiseLike<T6>, T7 | PromiseLike<T7>, T8 | PromiseLike<T8>]): Task<[T1, T2, T3, T4, T5, T6, T7, T8]>;
