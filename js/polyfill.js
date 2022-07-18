638c32e31d5e5431f5482c7180f6d1c586b84af2
Peter Jannesen <peter@jannesen.com>
2022-07-18 20:33:14 +0200

    Remove IE8/ES6 polyfills

--- js/polyfill.js
+++ js/polyfill.js
@@ -1,29 +1,4 @@
 ﻿"use strict;";
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// Polyfills
-//
-
-if (window.console === undefined) {
-    window.console = {
-        log:    function() {},
-        error:  function() {},
-        assert: function() {}
-    };
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// Typescript
-//
-var __extends = (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 
 var $global = {
     window:     window,
